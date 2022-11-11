477635045fd756faef3d654bb6b08b5d9fb4e656
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-11 17:22:06 +0100

    Upgrade images in js

--- js/main.js
+++ js/main.js
@@ -1,44 +1,69 @@
+// Creo una variabile per il nostro ".container".
+let contenitore = document.querySelector(".container");
+
+// Creo una variabile per il nostro ".carousel".
+let carousel = document.querySelector(".carousel");
+
+// Aggiungo il "carousel" all'interno del "contenitore".
+contenitore.append(carousel);
+
+// Creo una variabile per il nostro ".elements-container"
+let elementsContainer = document.querySelector(".elemets-container");
+
+// Aggiungo il "elementsContainer" all'interno del "carousel".
+carousel.append(elementsContainer);
+
+// Creo 5 "div" per quante saranno le immagini.
+let element = document.createElement("div");
+
+// Aggiungo una classe ai "div".
+element.classList.add("element");
+
+// Al "elementsContainer" vado ad aggiungerci i "div".
+elementsContainer.append(element);
+
 // Creo un variabile per il nostro Array.
 let arrayImages = [1,2,3,4,5];
 console.log(arrayImages);
 
-// Creo un ciclo per la nostra variabile "i" da "1" a "5".
-for( let i = 1; i <= arrayImages.length; i++){
-    console.log(i);
+// Creo la prima Immagine.
+let image = document.createElement("img");
 
-    // Creo una variabile per il nostro ".container".
-    let contenitore = document.querySelector(".container");
+// Inserisco l'immagine in "image"
+image.src = "./img/01.webp";
 
-    // Creo una variabile per il nostro ".carousel".
-    let carousel = document.querySelector(".carousel");
+// A "element" vado ad aggiungerci l'"img" al suo interno.
+element.append(image);
 
-    // Aggiungo il "carousel" all'interno del "contenitore".
-    contenitore.append(carousel);
+// Creo una variabile sull'"active".
+let activeElement = 1;
 
-    // Creo una variabile per il nostro ".elements-container"
-    let elementsContainer = document.querySelector(".elemets-container");
+// Collego il bottone "next".
+let myButtonUp = document. querySelector(".next");
 
-    // Aggiungo il "elementsContainer" all'interno del "carousel".
-    carousel.append(elementsContainer);
+// Creo l'interazione del bottone "myButtonUp".
+myButtonUp.addEventListener("click",
 
-    // Creo 5 "div" per quante saranno le immagini.
-    let element = document.createElement("div");
+    function(){ 
+        // Creo un ciclo per la nostra variabile "i" da "1" a "5".
+        for( let i = 1; i <= arrayImages; i++){
+        console.log(i);
 
-    // Aggiungo una classe ai "div".
-    element.classList.add("element");
+        image.src = "./img/02.webp";
 
-    // Al "elementsContainer" vado ad aggiungerci i "div".
-    elementsContainer.append(element);
+        image.src = "./img/03.webp";
+        
+        image.src = "./img/04.webp";
+        
+        image.src = "./img/05.webp";
+        
+        image.src = "./img/06.webp";
+        }
 
-    // Creo la prima Immagine.
-    let image01 = document.createElement("img");
-
-    // Inserisco l'immagine in "image"
-    image01.src = "./img/01.webp";
-
-    // A "element" vado ad aggiungerci l'"img" al suo interno.
-    element.append(image01);
-
-    image01 = [1];
+        if(activeElement < arrayImages - 1){
+            // Toglamo la classe "active" al elemento corrente.
+        }
     
-}
+    
+    }  
+);
