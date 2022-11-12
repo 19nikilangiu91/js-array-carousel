b1f344348369e70d278df2f2625b147d6d48c77c
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-12 16:18:31 +0100

    Button Next Done

--- js/main.js
+++ js/main.js
@@ -36,19 +36,56 @@ imageArray[4].src = 'img/05.webp';
 // Creo un ciclo per le immagini.
 
 for (let i = 0; i < imageArray.length; i++) {
-    let element = document.querySelector(".elemets-container");
+    let elementContainer= document.querySelector(".elemets-container");
     let div = document.createElement("div");
     div.classList.add("element");
     div.append (imageArray[i]);
-    element.append (div);
+    elementContainer.append (div);
 }
 
 // Colleghiamo al "div element" la classe "Active".
 
-let divElementArrey = document.querySelector(".element");
-divElementArrey.classList.add("active");
+let divElementArrey= document.getElementsByClassName("element");
+divElementArrey[0].classList.add("active");
 
-let ActiveIteam = 0;
+let activeIteam = 0;
+
+// Collego i bottoni "prev" e "next".
+let buttonPrev = document.querySelector(".prev");
+
+// il bottone prev da "hidden" diventerà "active".
+buttonPrev.classList.add("hidden")
+
+let buttonNext = document.querySelector(".next");
+
+// Aggiungiamo l'evento click al bottone "next".
+
+buttonNext.addEventListener("click",
+
+    function(){
+        if(activeIteam < divElementArrey.length - 1){
+            // Togliamo la classe "active" all'elemento corrente.
+            divElementArrey[activeIteam].classList.remove("active");
+
+            // Incrementiamo l'indice dell'elemento da visualizzare.
+            activeIteam ++;
+
+            // Aggiungiamo la classe "active" all'elemento successivo.
+            divElementArrey[activeIteam].classList.add("active");
+
+        };
+
+        if (activeIteam === divElementArrey.length - 1) {
+            buttonNext.classList.add("hidden");
+        }
+
+        if (activeIteam > 0) {
+            buttonPrev.classList.remove("hidden");
+            
+        }
+    }
+);
+
 
 
 
