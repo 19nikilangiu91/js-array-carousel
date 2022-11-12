9638e991988090cc7541903521f2c26a022b1c1b
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-12 01:04:14 +0100

    Milestone 2 Done

--- js/main.js
+++ js/main.js
@@ -13,55 +13,32 @@ let elementsContainer = document.querySelector(".elemets-container");
 // Aggiungo il "elementsContainer" all'interno del "carousel".
 carousel.append(elementsContainer);
 
-// Creo un variabile per il nostro Array.
-let arrayImages = [1,2,3,4,5];
-console.log(arrayImages);
+// Creo l'arrey per le nostre immagini.
 
-// Creo una variabile sull'"active".
-let activeElement = 1;
+let imageArray = [];
 
-// Collego il bottone "next".
-let myButtonUp = document. querySelector(".next");
+imageArray[0] = new Image();
+imageArray[0].src = 'img/01.webp';
 
-// Creo l'interazione del bottone "myButtonUp".
-myButtonUp.addEventListener("click",
+imageArray[1] = new Image();
+imageArray[1].src = 'img/02.webp';
 
-    function(){ 
-        // Creo un ciclo per la nostra variabile "i" da "1" a "5".
-        for( let i = 1; i <= arrayImages; i++){
-        console.log(i);
+imageArray[2] = new Image();
+imageArray[2].src = 'img/03.webp';
 
-        // Creo i "div".
-        let element = document.createElement("div");
+imageArray[3] = new Image();
+imageArray[3].src = 'img/04.webp';
 
-        // Aggiungo una classe ai "div".
-        element.classList.add("element");
+imageArray[4] = new Image();
+imageArray[4].src = 'img/05.webp';
 
-        // Creo la prima Immagine.
-        let image = document.createElement("img");
 
-        // A "element" vado ad aggiungerci l'"img" al suo interno.
-        element.append(image);
-                
-        // Inserisco l'immagine in "image".
-        image.src = "./img/01.webp";
-                
+// Creo un ciclo per le immagini.
 
-        // image.src = "./img/02.webp";
-
-        // image.src = "./img/03.webp";
-        
-        // image.src = "./img/04.webp";
-        
-        // image.src = "./img/05.webp";
-        
-        // image.src = "./img/06.webp";
-        }
-
-        // if(activeElement < arrayImages - 1){
-        //     // Toglamo la classe "active" al elemento corrente.
-        // }
-    
-    
-    }  
-);
+for (let i = 0; i < imageArray.length; i++) {
+    let element = document.querySelector(".elemets-container");
+    let div = document.createElement("div");
+    div.classList.add("element");
+    div.append (imageArray[i]);
+    element.append (div);
+}
